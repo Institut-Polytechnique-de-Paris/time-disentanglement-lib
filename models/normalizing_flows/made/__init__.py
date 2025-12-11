01004a6294c3b57ac5ea807d1d7919551e2e5bb0
KHALID OUBLAL <oublalkhalid@gmail.com>
2025-12-11 17:18:21 +0100

    add disoc

--- models/normalizing_flows/made/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Implementation of the Masked Autoencoder model (MADE) proposed
-in (https://arxiv.org/abs/1502.03509).
-"""
-
-from .made_config import MADEConfig
-from .made_model import MADE
-
-__all__ = ["MADE", "MADEConfig"]
--- pipelines/base_pipeline.py
+++
@@ -1,9 +0,0 @@
-class Pipeline:
-    def __init__(self):
-        pass
-
-    def __call__(self):
-        """
-        The method run the Pipeline process and must be implemented in a child class
-        """
-        raise NotImplementedError()
